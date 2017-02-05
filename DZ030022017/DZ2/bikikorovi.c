129ec3d46977b0d4391fd883568a30e2739108e8
introkun <introkun@gmail.com>
2017-02-05 21:51:46 +0300

    Update bikikorovi.c

--- DZ030022017/DZ2/bikikorovi.c
+++ DZ030022017/DZ2/bikikorovi.c
@@ -1,21 +1,13 @@
 #include "biki.h"
 
-int biki = 0;
-int korovi = 0;
-extern int secret;
-extern int secret1;
-extern int secret2;
-extern int secret3;
-extern int secret4;
-extern int chisloPolsovatelya;
-extern int chisloPolsovatelya1;
-extern int chisloPolsovatelya2;
-extern int chisloPolsovatelya3;
-extern int chisloPolsovatelya4;
+// Source файл должен называться так же как и header файл
 
 int bikiKorovi(int secret1, int secret2, int secret3, int secret4,
 	int chisloPolsovatelya1, int chisloPolsovatelya2, int chisloPolsovatelya3, int chisloPolsovatelya4)
 {
+	int biki = 0;
+        int korovi = 0;
+	
 	if (chisloPolsovatelya1 == secret1 || chisloPolsovatelya1 == secret2 ||
 		chisloPolsovatelya1 == secret3 || chisloPolsovatelya1 == secret4)
 	{
