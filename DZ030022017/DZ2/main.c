96a46af6e5fbbdb41e7224672509f458f7fb42d0
introkun <introkun@gmail.com>
2017-02-05 21:49:07 +0300

    Update main.c

--- DZ030022017/DZ2/main.c
+++ DZ030022017/DZ2/main.c
@@ -2,9 +2,8 @@
 #include <locale.h>
 #include <stdlib.h>
 #include <time.h>
+
 #include "biki.h"
-
-
 
 /*Задание 2
 Написать игру «Быки и коровы».Программа "загадывает" четырёхзначное число и
@@ -13,53 +12,43 @@
 (коровы).После отгадывания числа на экран необходимо вывести количество сделанных
 пользователем попыток.В программе необходимо использовать рекурсию.*/
 
-int bikiKorovi(int secret1, int secret2, int secret3, int secret4,
-	int chisloPolsovatelya1, int chisloPolsovatelya2, int chisloPolsovatelya3, int chisloPolsovatelya4);
-
-int chislo();
-int chisloPolsovatelya;
+int getNumberFromUser();
 
 int main()
 {
 	setlocale(LC_ALL, "rus");
+	
 	srand(time(0));
-	int secret = 1000+rand() % 9000;
-	int secret1 = secret / 1000;
-	int secret2 = secret / 100 % 10;
-	int secret3 = secret / 10 % 10;
-	int secret4 = secret % 10;
+	int const secret = 1000 + rand() % 9000;
+	int const secret1 = secret / 1000;
+	int const secret2 = secret / 100 % 10;
+	int const secret3 = secret / 10 % 10;
+	int const secret4 = secret % 10;
 
-	chisloPolsovatelya = chislo();
-	
-	
-	/*if (chisloPolsovatelya < 1000 || chisloPolsovatelya > 10000)
-	{
-		printf("Вы ввели не четырёхзначное число!\n");
-		chislo();
-		return 0;
-	}*/
+	int number = getNumberFromUser();
 
-	int chisloPolsovatelya1 = chisloPolsovatelya / 1000;
-	int chisloPolsovatelya2 = chisloPolsovatelya / 100 % 10;
-	int chisloPolsovatelya3 = chisloPolsovatelya / 10 % 10;
-	int chisloPolsovatelya4 = chisloPolsovatelya % 10;
+	int number1 = number / 1000;
+	int number2 = number / 100 % 10;
+	int number3 = number / 10 % 10;
+	int number4 = number % 10;
 
-	bikiKorovi(secret1, secret2, secret3, secret4,
-		chisloPolsovatelya1, chisloPolsovatelya2, chisloPolsovatelya3, chisloPolsovatelya4);
+	compareNumbers(secret1, secret2, secret3, secret4,
+		number1, number2, number3, number4);
 		
 	return 0;
 }
 
-int chislo()
+int getNumberFromUser()
 {
 	printf("Введите четырёхзначное число: \n");
-	scanf("%i", &chisloPolsovatelya);
+	int number;
+	scanf("%i", &number);
 
-	if (chisloPolsovatelya < 1000 || chisloPolsovatelya > 10000)
+	if (number < 1000 || number > 10000)
 	{
 		printf("Вы ввели не четырёхзначное число!\n");
 		chislo();		//рекурсия
 	}
 
-	return chisloPolsovatelya;
+	return number;
 }
